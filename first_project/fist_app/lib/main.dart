e872c3bc00369f74545a011f9e7de070320c0412
chris dexter <195526+the-dext@users.noreply.github.com>
2025-12-24 15:19:05 +0000

    .

--- first_project/fist_app/lib/main.dart
+++ first_project/fist_app/lib/main.dart
@@ -5,24 +5,33 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
+      home: const Scaffold(
         backgroundColor: Colors.amber,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepPurple, Colors.blueGrey, Colors.red],
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.blueGrey, Colors.red],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }

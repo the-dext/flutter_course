219b29a87733727b2f95af45854e5b091c1281bc
Chris Dexter <8195526+the-dext@users.noreply.github.com>
2026-01-06 18:08:11 +0000

    adding an expenses list

--- expense_tracker/expense_tracker/lib/expenses.dart
+++ expense_tracker/expense_tracker/lib/expenses.dart
@@ -2,6 +2,8 @@
 // chart
 // list of expenses
 
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -14,11 +16,55 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Tram ticket",
+      amount: 4.5,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "Lunch",
+      amount: 5,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Mobile phone",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.bill,
+    ),
+    Expense(
+      title: "Eating out",
+      amount: 42.23,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     // return the widget tree here.
     return Scaffold(
-      body: Column(children: [Text("the chart"), Text("the expenses list")]),
+      body: Column(
+        children: [
+          Text("the chart"),
+          ExpensesList(expenses: _registeredExpenses),
+        ],
+      ),
     );
   }
 }
---
+++ expense_tracker/expense_tracker/lib/expenses_list.dart
@@ -0,0 +1,26 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    // for an unknown number of expenses using a column is a bad decision,
+    // because all the items would be created but not visible.
+    // this costs performance.
+    // instead for this situation you can use a ListView widget.
+    // which is scrollable but this would still create all children.
+
+    // to get around that, user the builder constructor function so that it is
+    // scrollable but doesn't create the list items until they are visible
+    // or about to be visible.
+
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Text(expenses[index].title),
+    );
+  }
+}

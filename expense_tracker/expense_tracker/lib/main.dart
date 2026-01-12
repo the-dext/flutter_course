0e49350d937fa1fa46c5cd79710ae718eab17293
chris dexter <195526+the-dext@users.noreply.github.com>
2026-01-12 17:24:05 +0000

    started theming

--- expense_tracker/expense_tracker/lib/main.dart
+++ expense_tracker/expense_tracker/lib/main.dart
@@ -2,6 +2,35 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+// color scheme might not be newest way to theme
+// but it's one of the easiest.
+// you set up base colours and flutter infers the colours for widgets
+// from these base colours
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.all(16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            
+          ),
+        )
+      ),
+      home: Expenses(),
+    ),
+  );
 }

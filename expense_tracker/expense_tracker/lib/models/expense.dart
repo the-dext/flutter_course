351b6d19aed8e793286f4d63842356f430a6ba7a
chris dexter <195526+the-dext@users.noreply.github.com>
2026-01-15 18:52:19 +0000

    adding the expense bucket for charting.

--- expense_tracker/expense_tracker/lib/models/expense.dart
+++ expense_tracker/expense_tracker/lib/models/expense.dart
@@ -33,3 +33,18 @@ class Expense {
 
   String get formattedDate => formatDate(date);
 }
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+
+  ExpenseBucket({required this.category, required this.expenses});
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final ex in expenses) {
+      sum += ex.amount;
+    }
+    return sum;
+  }
+}
